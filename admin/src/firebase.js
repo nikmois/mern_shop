4d64f581986add2d7cad5e0b3430e8229653cfa3
Nikolai Moissejev <nik.moissejev@gmail.com>
2022-04-14 13:41:29 +0300

    minoe changes

--- admin/src/firebase.js
+++ admin/src/firebase.js
@@ -3,12 +3,12 @@ import firebase from "firebase/app";
 import 'firebase/storage';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBCWOsCG1M0jlqD9dp2LI7f33rPzYqk1_8",
-  authDomain: "babypingviin-51fca.firebaseapp.com",
-  projectId: "babypingviin-51fca",
-  storageBucket: "babypingviin-51fca.appspot.com",
-  messagingSenderId: "612373375213",
-  appId: "1:612373375213:web:e46e7ed9342ccf448a44e4"
+  apiKey: process.env.REACT_APP_FIRE_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOM,
+  projectId: process.env.REACT_APP_PROJ_ID,
+  storageBucket: process.env.REACT_APP_STOR_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 firebase.initializeApp(firebaseConfig);
 var storage = firebase.storage();
